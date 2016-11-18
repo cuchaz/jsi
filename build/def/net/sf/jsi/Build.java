21ee4ad17961d0ddeff409a9ee04000a9fbe2420
Jeff Martin <cuchaz@gmail.com>
2016-11-18 16:11:05 -0500

    switched to jerkar for builds

---
+++ build/def/net/sf/jsi/Build.java
@@ -0,0 +1,47 @@
+package net.sf.jsi;
+
+import org.jerkar.api.depmanagement.JkDependencies;
+import org.jerkar.api.depmanagement.JkModuleId;
+import org.jerkar.api.depmanagement.JkVersion;
+import org.jerkar.tool.builtins.javabuild.JkJavaBuild;
+import org.jerkar.tool.builtins.javabuild.JkJavaPacker;
+
+public class Build extends JkJavaBuild {
+	
+	@Override
+	public JkModuleId moduleId() {
+		return JkModuleId.of("cuchaz", "rtree-jsi");
+	}
+	
+	@Override
+	public JkVersion version() {
+		return JkVersion.ofName("1.0");
+	}
+
+	@Override
+	public String javaSourceVersion() {
+		return "1.8";
+	}
+	
+	@Override
+	public JkDependencies dependencies() {
+		return JkDependencies.builder()
+				
+			// test deps
+			.on("junit:junit:4.12").scope(TEST)
+				
+			// compile dependencies
+			.on("org.slf4j:slf4j-api:1.7.10")
+			.on("net.sf.trove4j:trove4j:3.0.3")
+			
+			.build();
+	}
+	
+	@Override
+	protected JkJavaPacker createPacker() {
+		return JkJavaPacker.builder(this)
+			.includeVersion(true)
+			.doSources(false)
+			.build();
+	}
+}
